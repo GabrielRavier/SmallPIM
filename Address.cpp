0a8c9c581d0d52ab488c606354fdc692f81a0fd4
Gabriel Ravier <31440290+GabrielRavier@users.noreply.github.com>
2018-03-15 21:42:41 +0100

    Made implementation of Address class

--- Address.cpp
+++ Address.cpp
@@ -1,1 +1,30 @@
 #include "Address.h"
+#include <cstring>
+
+using std::strcpy;
+
+Address::Address()
+{
+    // Initialise all strings to empty
+    m_lastname[0] = m_firstname[0] = m_phone[0] = m_address[0] = '\0';
+}
+
+void Address::lastname(const char *source)
+{
+    strcpy(m_lastname, source);
+}
+
+void Address::firstname(const char *source)
+{
+    strcpy(m_firstname, source);
+}
+
+void Address::phone(const char *source)
+{
+    strcpy(m_lastname, source);
+}
+
+void Address::address(const char *source)
+{
+    strcpy(m_address, source);
+}
