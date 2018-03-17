f3c820b039d4b25158657d38e2c9a5fca2e279a9
Gabriel Ravier <31440290+GabrielRavier@users.noreply.github.com>
2018-03-17 10:39:16 +0100

    Made test program for AddressBook class

--- main.cpp
+++ main.cpp
@@ -1,31 +1,66 @@
 #include <iostream>
 
-#include "Address.h"
+#include "AddressBook.h"
 
 using std::cout;
 using std::endl;
 
-void dump(const Address& a)
-{
-    cout << a.firstname() << ' '
-         << a.lastname() << '\n'
-         << a.address() << '\n'
-         << a.phone() << '\n'
-         << endl;
-}
-
 int main()
 {
+    AddressBook book;
+
     Address a;
     a.firstname("Gabriel");
     a.lastname("Ravier");
     a.phone("(617) 555-9876");
     a.address("The GDN Company\nSomewhere, MA 01000");
-    dump(a);
 
-    // Add phone extension
-    a.phone("(617) 555-7777 ext. 112");
-    dump(a);
+    Address b;
+    b.firstname("Blargo");
+    b.lastname("Balrog");
+    b.phone("(CS) 555-CAVE");
+    b.address("Nicalis\nSomewhere idk lol");
+
+    Address c;
+    c.lastname("Neighborhood Video");
+    c.phone("555-FILM");
+
+    int a_ID = book.insertAddress(a);
+    int b_ID = book.insertAddress(b);
+    int c_ID = book.insertAddress(c);
+
+    cout << "*** Three Address Entries ***\n";
+    book.print();
+
+    // Address d has same name as b
+    Address d;
+    d.firstname("Blargo");
+    d.lastname("Balrog");
+    d.phone("555-STORY");
+    d.address("1 Small St.\nMarlboro, MA 02100");
+
+    // Insert address with duplicate name
+    int d_ID = book.insertAddress(d);
+    cout << "*** After inserting a duplicate Blargo Balrog ***\n";
+    book.print();
+
+    // Erase an address
+    book.eraseAddress(a_ID);
+    cout << "*** After erasing Gabriel Ravier ***\n";
+    book.print();
+
+    // Replace an address
+    c.address("22 Main St.\n");
+    book.replaceAddress(c, c_ID);
+    cout << "*** After replacing Neighborhood Video ***\n";
+    book.print();
+
+    // Get and print address
+    const Address& d2 = book.getAddress(d_ID);
+    cout << "*** Copy of d: ***\n"
+         << d2.firstname() << ' ' << d2.lastname() << '\n'
+         << d2.address() << '\n' << d2.phone() << '\n'
+         << endl;
 
     return 0;
 }
