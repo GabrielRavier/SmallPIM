39fd42b1bc03c7001c001e908b0b5dbca7848d91
Gabriel Ravier <31440290+GabrielRavier@users.noreply.github.com>
2018-03-18 19:34:01 +0100

    Changed from vector to list implementation

--- include/AddressBook.h
+++ include/AddressBook.h
@@ -20,7 +20,7 @@ public:
     void eraseAddress(int recordID) throw (AddressNotFound);
     void replaceAddress(const Address& addr, int recordID = 0)
     throw (AddressNotFound);
-    const Address& getAddress(int recordID) const
+    const Address& getAddress(int recordID)
     throw (AddressNotFound);
 
     // Test routine to print out contents of address book
@@ -32,7 +32,17 @@ private:
     AddressBook& operator=(const AddressBook&);
 
     static int m_nextID;
-    std::list<Address> m_addresses;
+
+    // Define addrlist as a shorthand for std::list<Address>
+    typedef std::list<Address> addrlist;
+    addrlist m_addresses;
+
+    // Get the index of the record with the specified ID.
+    // Returns end() if not found.
+    addrlist::iterator  getByID(int recordID)
+        throw (AddressNotFound);
+    addrlist::const_iterator  getByID(int recordID) const
+        throw (AddressNotFound);
 };
 
 #endif // ADDRESSBOOK_H
